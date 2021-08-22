08710db172b49fb22ae1e5057f2effb803d221b4
jasim741891 <jasimgai74@gmail.com>
2021-08-22 19:49:13 +0600

    functional bapper bank

--- js/another.js
+++ js/another.js
@@ -6,19 +6,29 @@ function getInputValue(fieldId) {
     return inputAmount;
 };
 function updateTotal(fieldId, amount) {
-    console.log(fieldId, amount)
-    const totalTag = document.getElementById(fieldId);
-    const totalText = totalTag.innerText;
-    const totalAmount = parseFloat(totalText);
+
+    // const totalTag = document.getElementById(fieldId);
+    // const totalText = totalTag.innerText;
+    // const totalAmount = parseFloat(totalText);
+    const totalAmount = getInputInnerTex(fieldId)
     const newTotal = totalAmount + amount
-    totalTag.innerText = newTotal;
+    document.getElementById(fieldId).innerText = newTotal;
     return newTotal;
 
 };
+function getInputInnerTex(fieldId) {
+    const innerTag = document.getElementById(fieldId);
+    const innerInputText = innerTag.innerText;
+    const innerAmount = parseFloat(innerInputText);
+    return innerAmount;
+}
 function updateBalance(amount, isadd) {
-    const balanceTag = document.getElementById('balance-total');
-    const balanceText = balanceTag.innerText;
-    const balanceAmount = parseFloat(balanceText);
+    // const balanceTag = document.getElementById('balance-total');
+    // const balanceText = balanceTag.innerText;
+    // const balanceAmount = parseFloat(balanceText);
+
+
+    const balanceAmount = getInputInnerTex('balance-total')
     let newUpdateBalance;
     if (isadd == true) {
         newUpdateBalance = balanceAmount + amount;
@@ -27,18 +37,23 @@ function updateBalance(amount, isadd) {
         newUpdateBalance = balanceAmount - amount;
     }
 
-    balanceTag.innerText = newUpdateBalance;
+    document.getElementById('balance-total').innerText = newUpdateBalance;
 
 }
 
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total', amount);
-    updateBalance(amount, true)
+    if (amount > 0) {
+        updateTotal('deposit-total', amount);
+        updateBalance(amount, true)
+    }
 });
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const amount = getInputValue('withdraw-input');
-    updateTotal('withdraw-total', amount)
-    updateBalance(amount, false)
+    const balanceTotalUp = getInputInnerTex('balance-total')
+    if (amount > 0 && amount <= balanceTotalUp) {
+        updateTotal('withdraw-total', amount)
+        updateBalance(amount, false)
+    }
 })
